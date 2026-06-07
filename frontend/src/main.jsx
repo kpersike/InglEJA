caeb8adacb5da43d85b46093784860be0c152c72
kpersike <kaikpersikemai@gmail.com>
2026-06-06 22:49:38 -0300

    fix: GoogleOAuth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="636993740599-r7j70hbpj99dlni9ptknu5f3eb1klnd6.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="926852175310-p6atgp43jeciqa16c6v34hv77a9710lt.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>,
